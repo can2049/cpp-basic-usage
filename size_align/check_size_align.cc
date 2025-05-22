9adcd3dff28c100c9da910c48483729adf378847
CanWang <canwhut@gmail.com>
2025-05-22 16:49:38 +0800

    update sizeof alignof

--- size_align/check_size_align.cc
+++ size_align/check_size_align.cc
@@ -16,6 +16,10 @@ struct Point4 {
   int x, y, z, w;
 };
 
+struct Point5 {
+  int x, y, z, w, v;
+};
+
 int main() {
   std::cout << "sizeof(Point1): " << sizeof(Point1) << std::endl;
   std::cout << "alignof(Point1): " << alignof(Point1) << std::endl;
@@ -25,4 +29,6 @@ int main() {
   std::cout << "alignof(Point3): " << alignof(Point3) << std::endl;
   std::cout << "sizeof(Point4): " << sizeof(Point4) << std::endl;
   std::cout << "alignof(Point4): " << alignof(Point4) << std::endl;
+  std::cout << "sizeof(Point5): " << sizeof(Point5) << std::endl;
+  std::cout << "alignof(Point5): " << alignof(Point5) << std::endl;
 }
