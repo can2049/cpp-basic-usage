128afa960be2fe2698488d2ef16f0dedaa394617
CanWang <canwhut@gmail.com>
2025-05-22 16:48:44 +0800

    add case

--- class_demo/simple_class.cc
+++ class_demo/simple_class.cc
@@ -12,10 +12,30 @@ class baseB {
   int _md = 4;
 };
 
-// derived class
-class deriveA : public baseA, public baseB {
+class baseC {
  public:
-  virtual void print() {}
+  virtual void func_from_c() {}
+  int _me = 5;
+  int _mf = 6;
+};
+
+class baseD {
+ public:
+  virtual void func_from_d() {}
+  int _mg = 7;
+  int _mh = 8;
+};
+
+// derived class
+class deriveA : public baseA, public baseB, public baseC, public baseD {
+ public:
+  virtual void final_func() {}
+  virtual void func_from_d() override {
+    // do something
+  }
+  virtual void func_from_c() override {
+    // do something
+  }
   int _me = 3;
   int _mf = 4;
 };
---
+++ size_align/check_size_align.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct Point1 {
+  int x;
+};
+
+struct Point2 {
+  int x, y;
+};
+
+struct Point3 {
+  int x, y, z;
+};
+
+struct Point4 {
+  int x, y, z, w;
+};
+
+int main() {
+  std::cout << "sizeof(Point1): " << sizeof(Point1) << std::endl;
+  std::cout << "alignof(Point1): " << alignof(Point1) << std::endl;
+  std::cout << "sizeof(Point2): " << sizeof(Point2) << std::endl;
+  std::cout << "alignof(Point2): " << alignof(Point2) << std::endl;
+  std::cout << "sizeof(Point3): " << sizeof(Point3) << std::endl;
+  std::cout << "alignof(Point3): " << alignof(Point3) << std::endl;
+  std::cout << "sizeof(Point4): " << sizeof(Point4) << std::endl;
+  std::cout << "alignof(Point4): " << alignof(Point4) << std::endl;
+}
